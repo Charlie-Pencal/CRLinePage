185d06a8fb540e8a99c5f6fc6a8c179987e00cc0
Juliane <julianebueno1@gmail.com>
2023-10-19 16:48:08 -0300

    feat: conexão do estilo global e tema no arquivo de Rotas

--- src/style/GlobalStyle.js
+++ src/style/GlobalStyle.js
@@ -1,26 +1,30 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    font-family: 'Red Hat Display', sans-serif;
-  }
-  html, :root {
-    width: 100%;
-    min-height: 100vh;
-  }
-  button,
-  a {
-    cursor: pointer;
-  }
-  a, li{
-    text-decoration: none;
-    list-style: none;
-    color: inherit;
-  }
-  button, input {
-    outline: none;
-  }
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: 'Red Hat Display', sans-serif;
+        color: ${(props) => props.theme.branco};
+    }
+    html, :root {
+        width: 100%;
+        min-height: 100vh;
+    }
+    body{
+        background-color: ${(props) => props.theme.azul};
+    }
+    button, a {
+        cursor: pointer;
+        color: inherit;
+    }
+    a, li{
+        text-decoration: none;
+        list-style: none;
+        color: inherit;
+    }
+    button, input {
+        outline: none;
+    }
 `;
--- src/style/tema.js
+++ src/style/tema.js
@@ -1,8 +1,7 @@
 export const tema = {
-  azul: "#03273B",
-  dourado: "#FFB27D",
-  dourado2: "#B88E2F",
-  marrom: "#250E11",
-  branco: "#FEFEFE",
-  preto: "#080808",
+    azul: "#03273B",
+    areia: "#FFB27D",
+    marrom: "#250E11",
+    branco: "#FEFEFE",
+    preto: "#080808",
 };
