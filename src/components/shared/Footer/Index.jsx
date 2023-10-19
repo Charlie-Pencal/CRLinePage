ae183dc9f6920f8be50cdd548951d2f9c82a3163
emerchagas <chseeu@gmail.com>
2023-10-19 18:53:11 -0300

    feat: Criação do Footer

---
+++ src/components/shared/Footer/Index.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { StyledFooter } from "./style";
+
+
+
+const Footer = () => {
+  return (
+<StyledFooter>
+    <div className="footer-container">
+      <div className="footer-content">
+        <p>
+          Av. Sete de Setembro, 2775 - Rebouças, Curitiba - PR,
+          <br /> 80230-010
+        </p>
+        <ul className="footer-list">
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/comprar">Shop</a>
+          </li>
+          <li>
+            <a href="/sobre">Sobre a CRLine</a>
+          </li>
+          <li>
+            <a href="/contato">Fale Conosco</a>
+          </li>
+        </ul>
+
+        <ul className="footer-list">
+          <li>
+            <a href="/payment-options">Pagamentos</a>
+          </li>
+          <li>
+            <a href="/returns">Devolução</a>
+          </li>
+          <li>
+            <a href="/privacy-policies">Politica de privacidade</a>
+          </li>
+        </ul>
+
+        <div className="footer-logo">
+          <img src="/logo.svg" alt="Logo" />
+        </div>
+      </div>
+
+      <hr />
+
+      <div className="footer-direitos">
+        <p>2023 CRLine. Todos os direitos Reservados.</p>
+      </div>
+    </div>
+    </StyledFooter>
+  );
+}
+
+export default Footer;
---
+++ src/components/shared/Footer/style.js
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+export const StyledFooter = styled.footer`
+
+.footer-container{
+    
+    height: 150px;
+    position: relative;
+    
+    
+    }
+    
+    .footer-content{
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+            
+    }
+    p{
+        margin-top: 48px;
+        Border:1px;
+    }
+    
+    img{
+        width: 261px;
+        height: 250px;
+        
+    }
+    
+    .footer-list{
+    font-family: Poppins;
+    font-size: 16px;
+    line-height: 44px;
+    list-style: none;
+    
+    }
+    
+    .footer-direitos{
+    padding-left: 40px;
+    }`
