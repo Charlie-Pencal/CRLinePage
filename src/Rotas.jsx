1b5fb06a023bba85c7ea04dccc775f9295611d01
Juliane <julianebueno1@gmail.com>
2023-10-20 18:24:28 -0300

    feat: criação da estrutura básica das Rotas

--- src/Rotas.jsx
+++ src/Rotas.jsx
@@ -1,24 +1,48 @@
-import { GlobalStyle } from './style/GlobalStyle'
-import { ThemeProvider } from 'styled-components'
-import { tema } from './style/tema'
-import Button from './components/common/Button';
-import Input from './components/common/Input/Index';
-import Cards from './components/common/Cards';
-import { Header } from './components/shared/Header/Index';
-
-
+import { GlobalStyle } from "./style/GlobalStyle";
+import { ThemeProvider } from "styled-components";
+import { tema } from "./style/tema";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const Rotas = () => {
     return (
-        <ThemeProvider theme={tema} >
-            <GlobalStyle/>
-            
-            
-            <Header/>
-        </ThemeProvider>
+        <BrowserRouter>
+            <ThemeProvider theme={tema}>
+                <GlobalStyle />
+                <Routes>
+                    {/* Rotas aguardando a criação das Páginas/Elementos */}
 
-        
+                {/* ANDRE */}
+                    {/* rota para a Home */}
+                    <Route path="/" element={<></>} />
+                    {/* rota para Catalogo */}
+                    <Route path="/catalogo" element={<></>} />
+
+                {/* LETICIA */}
+                    {/* rota para Produto */}
+                    <Route path="/produto" element={<></>} />
+
+                {/* CHARLIE */}
+                    {/* rota para Login */}
+                    <Route path="/login" element={<></>} />
+                    {/* rota para Cadastro */}
+                    <Route path="/cadastro" element={<></>} />
+
+                {/* EMERSON */}
+                    {/* rota para Carrinho */}
+                    <Route path="/carrinho" element={<></>} />
+                    {/* rota para Checkout */}
+                    {/* <Route path="/checkout" element={<></>} /> */}
+
+                {/* JULIANE */}
+                    {/* rota para Perfil */}
+                    <Route path="/perfil" element={<></>} />
+                    {/* rota para Pedidos */}
+                    {/* <Route path="/pedidos" element={<></>} /> */}
+
+                </Routes>
+            </ThemeProvider>
+        </BrowserRouter>
     );
-}
+};
 
 export default Rotas;
