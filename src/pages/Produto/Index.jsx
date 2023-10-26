7ad132932916c6c695748d62970428c4ffd9fd47
André Leodoro <aleodoro93@gmail.com>
2023-10-26 02:59:06 -0300

    feat:criação do botão para call to action na pagina do produto/ alteração da estilização do botao de variante quatro.

--- src/pages/Produto/Index.jsx
+++ src/pages/Produto/Index.jsx
@@ -77,7 +77,7 @@ const Produto = () => {
                             <Button variante={"segundo"} texto={"A6"}/>
                         </div>
                         <div className="botoesCompras">
-                            <Button variante={"quarto"} texto={"Adicionar ao Carrinho"} onClick={handleAdicionarCarrinho}/>
+                            <Button variante={"quinto"} texto={"Adicionar ao Carrinho"} onClick={handleAdicionarCarrinho}/>
                             <Button variante={"quarto"} texto={"Comprar"} onClick={handleComprar}/>
                         </div>
                     </div>
@@ -88,10 +88,7 @@ const Produto = () => {
                     <h2>Descrição Adicional</h2>
                     <p>Embodying the raw, wayward spirit of rock n roll, the Kilburn portable active stereo speaker takes the unmistakable look and sound of Marshall, unplugs the chords, and takes the show on the road.</p>
                     <p>Weighing in under 7 pounds, the Kilburn is a lightweight piece of vintage styled engineering. Setting the bar as one of the loudest speakers in its class, the Kilburn is a compact, stout-hearted hero with a well-balanced audio which boasts a clear midrange and extended highs for a sound that is both articulate and pronounced. The analogue knobs allow you to fine tune the controls to your personal preferences while the guitar-influenced leather strap enables easy and stylish travel.</p>
-                    <div>
-                        <img src="/product.jpg" alt="" />
-                        <img src="/product.jpg" alt="" />
-                    </div>
+                    
 
                 </div>
             
