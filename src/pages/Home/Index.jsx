e9d8961dfa13a8248d003ba0f5ce532c67eea243
André Leodoro <aleodoro93@gmail.com>
2023-10-26 02:12:17 -0300

    feat: adição da funcionalidade do botão da home e adição da foto real dos produtos da home/remoção do preço de desconto de produtos da pagina shop

--- src/pages/Home/Index.jsx
+++ src/pages/Home/Index.jsx
@@ -3,11 +3,18 @@ import { Header } from '../../components/shared/Header/Index'
 import * as S from "./Style"
 import Button from '../../components/common/Button'
 import Sketchbook from "/product.jpg"
-import Grimorio from "/product.jpg"
-import Tematico from "/product.jpg"
+import Grimorio from "/grimorio.jpg"
+import Tematico from "/tematicos.jpg"
 import Footer from '../../components/shared/Footer/Index'
 import { tema } from '../../style/tema'
 const Home = () => {
+
+    const scrollSection = () =>{
+        const section =  document.querySelector('#produtos');
+        section.scrollIntoView({behavior: 'smooth'})
+    }
+
+
     return (
         <>
             <Header />
@@ -17,12 +24,12 @@ const Home = () => {
                     <h1 className='text1'>Conheça nossa nova linha de Midoris</h1>
                     <h2 className='text2'>Troque os miolos sempre que quiser e precisar.</h2>
                     <div className='btnDiv'>
-                        <Button variante={"quarto"} texto={"Conheça já"}/>
+                        <Button variante={"quarto"} texto={"Conheça já"} onClick={scrollSection}/>
                     </div>
                 </div>
             </S.SectionStyled>
 
-            <S.SectionProducts>
+            <S.SectionProducts id='produtos'>
                 <div className='divText'>
                     <h1 className='text3'>Conheça nossos produtos</h1>
                     <p className='text4'>Temos Cadernos, sketchbooks, agendas, midoris e grimórios</p>
