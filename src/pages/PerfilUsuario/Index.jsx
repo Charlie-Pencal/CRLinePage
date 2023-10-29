2a1e04002f29e8170fc7cfa069aafd54ebbc6161
Charlie-Pencal <cpencal@gmail.com>
2023-10-28 22:13:19 -0300

    feat: Criado modal

--- src/pages/PerfilUsuario/Index.jsx
+++ src/pages/PerfilUsuario/Index.jsx
@@ -2,16 +2,22 @@ import { useEffect, useState } from 'react'
 import { Header } from "../../components/shared/Header/Index";
 import Footer from "../../components/shared/Footer/Index";
 import { StyledPerfilUsuario } from "./style";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import axios from 'axios';
+import Button from '../../components/common/Button';
+import Modal from '../../components/common/Modal';
+import Input from '../../components/common/Input/Index';
 
 const PerfilUsuario = () => {
     
+    const params = useParams()
     // depois alterar idCliente para consumir o local storage
     const idCliente = JSON.parse(localStorage.getItem('userId'))
     const urlApi = `http://localhost:3000/clientes/${idCliente}`
 
     const [cliente, setCliente] = useState([])
+
+    const [modalAberto, setModalAberto] = useState(false)
 
     const getCliente = async() => {
         try {
@@ -55,10 +61,39 @@ const PerfilUsuario = () => {
                                 <p>{cliente.email}</p>
                             </div>
                         </div>
+                        <Button variante={"quinto"} texto={"Editar Dados Pessoais"} onClick={() => setModalAberto(true)}/>
                     </div>
                 </div>
             </StyledPerfilUsuario>
             <Footer />
+
+            <Modal title={params.tipo} open={modalAberto} fechar={() => setModalAberto(false)}>
+                <h1>Alteração dos Dados Pessoais</h1>
+                <label htmlFor="">Nome</label>
+                <Input 
+                type="text"
+                width='50%'
+                height='45px'
+                radius='8px'
+                padding={'10px'}
+                  />
+                <label htmlFor="">Sobrenome</label>
+                <Input
+                type="text"
+                width='50%'
+                height='45px'
+                radius='8px'
+                padding='10px'
+                  />
+                <label htmlFor="">Email</label>
+                <Input
+                type="text"
+                width='100%'
+                height='45px'
+                radius='8px'
+                padding='10px'
+                  />
+            </Modal>
         </>
     );
 };
